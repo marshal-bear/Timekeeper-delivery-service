231e2fcbf9129c3e6e6d6199f70e6c52c0940b69
marshal-bear <71656300+marshal-bear@users.noreply.github.com>
2021-04-28 21:31:13 +0100

    Add files via upload

---
+++ work.js
@@ -0,0 +1,6 @@
+document.querySelector("head")
+document.querySelector("body")
+document.querySelector("#header")
+
+document.querySelector("#header > div")
+document.querySelector("#header > div > div")
